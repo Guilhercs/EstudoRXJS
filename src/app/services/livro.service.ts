0267845c28b05cae2ceed4ee9fbdb4ff5b38acd7
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-02-04 12:38:09 -0300

    RXJS operators, pipes angular

--- src/app/services/livro.service.ts
+++ src/app/services/livro.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LivroService {
+  private dataBase = 'https://www.googleapis.com/books/v1/volumes';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  getBooks(value: string): Observable<any>{
-    const params = new HttpParams().append('q', value)
-    return this.http.get('https://www.googleapis.com/books/v1/volumes', {params})
+  getBooks(value: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', value);
+    return this.http
+      .get<LivrosResultado>(this.dataBase, { params }).pipe(
+        tap(res => console.log(res)),
+        map(res => res.items),
+        tap(res => console.log(res)
+        ));
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,19 +1,35 @@
+import { Livro, Item } from './../../models/interfaces';
 import { LivroService } from './../../services/livro.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { VolumeInfo } from 'src/app/models/volumeInfo';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
-  listaLivros: [];
+export class ListaLivrosComponent implements OnDestroy {
+  listaLivros!: Livro[];
   search: string = '';
+  subscription: Subscription;
+  livro: Livro;
   constructor(private booksService: LivroService) {}
 
   getBooks() {
-    this.booksService.getBooks(this.search).subscribe({
-      next: listaLivros => this.listaLivros = listaLivros.items,
-    }
-  )}
+    this.subscription = this.booksService.getBooks(this.search).subscribe({
+      next: (items) => {
+        this.listaLivros = this.booksResults(items);
+      },
+      error: (erro) => console.log(erro),
+    });
+  }
+  booksResults(items: Item[]): VolumeInfo[] {
+    return items.map(item => {
+      return new VolumeInfo(item)
+    })
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
