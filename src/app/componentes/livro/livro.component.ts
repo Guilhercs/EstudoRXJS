78b00d2b92bbeff8f78304b3e9eb62a85670ef20
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-02-05 21:22:08 -0300

    create pipes

--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -4,12 +4,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-livro',
   templateUrl: './livro.component.html',
-  styleUrls: ['./livro.component.css']
+  styleUrls: ['./livro.component.css'],
 })
 export class LivroComponent {
-
   @Input() livro: Livro;
   modalAberto: boolean;
+  bookName: any;
 
   onModalChange(evento: boolean) {
     this.modalAberto = evento;
