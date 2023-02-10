afd6d4afd0f8c835f859b2bfe50b581205b86c3b
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-02-09 23:37:14 -0300

    quantidade de items encontrados

--- src/app/services/livro.service.ts
+++ src/app/services/livro.service.ts
@@ -10,13 +10,8 @@ export class LivroService {
   private dataBase = 'https://www.googleapis.com/books/v1/volumes';
   constructor(private http: HttpClient) {}
 
-  getBooks(value: string): Observable<Item[]> {
+  getBooks(value: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', value);
-    return this.http
-      .get<LivrosResultado>(this.dataBase, { params }).pipe(
-        tap(res => console.log(res)),
-        map(res => res.items),
-        tap(res => console.log(res)
-        ));
+    return this.http.get<LivrosResultado>(this.dataBase, { params })
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,17 @@
-import { Item } from './../../models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
 import { LivroService } from './../../services/livro.service';
 import { Component } from '@angular/core';
-import { catchError, debounceTime, distinctUntilChanged, EMPTY, filter, map, switchMap, throwError } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  EMPTY,
+  filter,
+  map,
+  of,
+  switchMap,
+  throwError,
+} from 'rxjs';
 import { VolumeInfo } from 'src/app/models/volumeInfo';
 import { FormControl } from '@angular/forms';
 
@@ -12,19 +22,38 @@ import { FormControl } from '@angular/forms';
 })
 export class ListaLivrosComponent {
   search = new FormControl();
-  msgError = ''
+  msgError = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private booksService: LivroService) {}
+  totalDeLivros$ = this.search.valueChanges.pipe(
+    debounceTime(300),
+    filter((value) => value.length >= 3),
+    distinctUntilChanged(),
+    switchMap((value) => this.booksService.getBooks(value)),
+    map(result => this.livrosResultado = result),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+    })
+  );
 
   livrosEncontrados$ = this.search.valueChanges.pipe(
     debounceTime(300),
     filter((value) => value.length >= 3),
     distinctUntilChanged(),
     switchMap((value) => this.booksService.getBooks(value)),
+    map((res) => res.items ?? []),
     map((items) => this.booksResults(items)),
     catchError(() => {
-      throwError(()=> new Error(this.msgError = 'Ops, ocorreu um erro'))
-      return EMPTY
+      throwError(
+        () =>
+          new Error(
+            (this.msgError =
+              'Ops, ocorreu um erro. Por favor, recarregue a aplicação')
+          )
+      );
+      return EMPTY;
     })
   );
 
